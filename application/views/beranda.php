83a1d2db1935bc4e81c5a5351c02bd2efb3c355f
riswanto84 <riswanto.aris@gmail.com>
2018-12-31 11:43:21 +0700

    31 12 18 11.43

--- application/views/beranda.php
+++ application/views/beranda.php
@@ -2,21 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-<script>
-        // fungsi initialize untuk mempersiapkan peta
-        function initialize() {
-        var propertiPeta = {
-            center:new google.maps.LatLng(-8.5830695,116.3202515),
-            zoom:9,
-            mapTypeId:google.maps.MapTypeId.ROADMAP
-        };
-        
-        var peta = new google.maps.Map(document.getElementById("googleMap"), propertiPeta);
-        }
-
-        // event jendela di-load  
-        google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
 
 <!-- Page Content -->
 <div class="container">
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>gunungpantai.com - Tugas Semantic Web kelompok : Kami        -MIK UI 2018-</title>
+    <title>wisata gunung - Tugas Semantic Web kelompok : Kami        -MIK UI 2018-</title>
 
     <!-- Bootstrap core CSS -->
     <link href="<?php echo base_url("bootstrap/vendor/bootstrap/css/bootstrap.min.css"); ?>"
@@ -28,19 +28,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">gunungpantai.com</a>
+        <a class="navbar-brand" href="<?php echo site_url(); ?>">wisata gunung Indonesia</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="#">Beranda
+              <a class="nav-link" href="<?php echo site_url(); ?>">Beranda
                 <span class="sr-only">(current)</span>
               </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Tentang</a>
             </li>
           </ul>
         </div>
