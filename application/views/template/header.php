fe43025bb2c32e6fb6640df13937fab1a933651c
riswanto84 <riswanto.aris@gmail.com>
2018-12-31 11:47:55 +0700

    311218 11.47

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -11,7 +11,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>wisata gunung - Tugas Semantic Web kelompok : Kami        -MIK UI 2018-</title>
+    <title>wisata gunung pantai - Tugas Semantic Web kelompok : Kami        -MIK UI 2018-</title>
 
     <!-- Bootstrap core CSS -->
     <link href="<?php echo base_url("bootstrap/vendor/bootstrap/css/bootstrap.min.css"); ?>"
@@ -28,7 +28,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="<?php echo site_url(); ?>">wisata gunung Indonesia</a>
+        <a class="navbar-brand" href="<?php echo site_url(); ?>">wisata gunung pantai Indonesia</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
